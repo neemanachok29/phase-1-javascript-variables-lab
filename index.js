4fb029600e1b0605422c5b064f56696a4b803892
Neema <neemanachok29@gmail.com>
2023-06-26 12:10:58 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
-// Code your solution in this file!
+// let companyName = 'Scuber';
+
+function companyName(){
+    const companyName = 'Scuber'
+}
+
+var mostProfitableNeighborhood = 'Chelsea';
+
+function mostProfitableNeighborhood(){
+    let mostProfitableNeighborhood = 'Chelsea';
+}
+
+var companyCeo = 'Susan smith'
+function companyCeo(){
+    let companyCeo = 'Susan smith';
+    return companyCeo;
+}
+
+
